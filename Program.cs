c59eec3ef9c3edac1376b7b7a76cc953f46bfd58
Haikal Izzuddin <heywhite69@gmail.com>
2016-11-17 22:06:52 +0800

    Remove old form

--- Program.cs
+++ Program.cs
@@ -17,7 +17,6 @@ namespace Reactive
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
         }
     }
 }
