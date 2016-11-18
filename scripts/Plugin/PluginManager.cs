a23298f19035f9f06db79ee3ec52800cf04f2cdb
Haikal Izzuddin <heywhite69@gmail.com>
2016-11-18 22:53:43 +0800

    PluginManager: Allow user/system to load/unload modules selectively

--- scripts/Plugin/PluginManager.cs
+++ scripts/Plugin/PluginManager.cs
@@ -1,9 +1,9 @@
 ﻿using PluginCore;
 using Reactive.Framework.Error;
-using System.Collections;
-using System.Collections.Generic;
+using System.Linq;
 using System.ComponentModel.Composition;
 using System.ComponentModel.Composition.Hosting;
+using System.Collections.Generic;
 
 namespace Reactive.Plugin
 {
@@ -16,16 +16,28 @@ namespace Reactive.Plugin
         internal string pluginPath = App.StartupPath + "\\plugins";
 
         /// <summary>
-        /// Public variable to allow access to list of plugins.
+        /// Public variable to allow access to list of available plugins.
         /// </summary>
         [ImportMany(typeof(IPlugin))]
         public IEnumerable<IPlugin> pluginsList;
 
         /// <summary>
+        /// A list of loaded plugins
+        /// </summary>
+        public List<IPlugin> loadedPlugins = new List<IPlugin>();
+
+        /// <summary>
         /// Initializes the plugin manager. Loads the list of plugins.
         /// </summary>
-        // todo: Find a way to load specific modules
         public PluginManager()
+        {
+            this.LoadAvailablePlugins();
+        }
+
+        /// <summary>
+        /// Loads the available plugins in the plugin directory
+        /// </summary>
+        public void LoadAvailablePlugins()
         {
             var catalog = new AggregateCatalog();
             catalog.Catalogs.Add(new DirectoryCatalog(pluginPath));
@@ -34,12 +46,50 @@ namespace Reactive.Plugin
         }
 
         /// <summary>
+        /// Allows the user/system to selectively load plugins into the system
+        /// </summary>
+        /// <param name="plugin"></param>
+        public void LoadPlugin(IPlugin plugin)
+        {
+            try
+            {
+                if (!pluginsList.Contains(plugin))
+                    throw new KeyNotFoundException();
+
+                loadedPlugins.Add(plugin);
+            }
+            catch (KeyNotFoundException e)
+            {
+                Debug.Log("Unable to load plugin: " + plugin.pluginName);
+            }
+        }
+
+        /// <summary>
+        /// Allows the user/system to selectively unload plugins into the system
+        /// </summary>
+        /// <param name="plugin"></param>
+        public void UnloadPlugin(IPlugin plugin)
+        {
+            try
+            {
+                if (!loadedPlugins.Contains(plugin))
+                    throw new KeyNotFoundException();
+
+                loadedPlugins.Remove(plugin);
+            }
+            catch (KeyNotFoundException e)
+            {
+                Debug.Log("Unable to unload plugin: " + plugin.pluginName);
+            }
+        }
+
+        /// <summary>
         /// Adds the Start, Stop, and Loop functions from the modules to the job handler
         /// </summary>
         /// todo: Find a way to run this at start of Application
         public void StartHost()
         {
-            foreach (var module in pluginsList)
+            foreach (var module in loadedPlugins)
             {
                 Framework.Jobs.JobHandler.AddToStart(module.Start);
                 Framework.Jobs.JobHandler.AddToLoop(module.Tick);
---
+++ scripts/Plugin/PluginSerializer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reactive.Plugin
+{
+    /// <summary>
+    /// Handles the custom plugin format. Basically it's a zip file with a custom extension.
+    /// </summary>
+    public class PluginSerializer
+    {
+    }
+}
