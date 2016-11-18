fa94adb1af854a3e76299a9bfeacd01f69642d11
Haikal Izzuddin <heywhite69@gmail.com>
2016-11-18 12:50:30 +0800

    Utils: Introduce GeneralExtensions

---
+++ scripts/Utils/Extensions/GeneralExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reactive.Utils.Extensions
+{
+    public static class GeneralExtensions
+    {
+        /// <summary>
+        /// Checks if class contains property.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="propertyName"></param>
+        /// <returns>True if class does contain property. False for otherwise</returns>
+        public static bool HasProperty(this Type obj, string propertyName)
+        {
+            return obj.GetProperty(propertyName) != null;
+        }
+    }
+}
