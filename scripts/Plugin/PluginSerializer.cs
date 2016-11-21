202590a031b3ecd624aae84d899720212e156169
Haikal Izzuddin <heywhite69@gmail.com>
2016-11-21 21:21:47 +0800

    Plugins: Serializer: Trying a new logic of handling plugin data

--- scripts/Plugin/PluginSerializer.cs
+++ scripts/Plugin/PluginSerializer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,31 @@ namespace Reactive.Plugin
     /// <summary>
     /// Handles the custom plugin format. Basically it's a zip file with a custom extension.
     /// </summary>
+    /// todo: Find a better logic for PluginSerializer to build custom modules data files. For now, this is not usable.
     public class PluginSerializer
     {
+        internal string pluginPath = App.StartupPath + "\\plugins\\data\\";
+        public List<string> loadedDataFiles = new List<string>();
+
+        private void LoadDataFile(string plugin)
+        {
+            try
+            {
+                if (File.Exists(pluginPath + plugin + ".data"))
+                {
+                    loadedDataFiles.Add(plugin);
+                }
+                else
+                {
+                    throw new FileNotFoundException();
+                }
+            }catch (FileNotFoundException e1)
+            {
+                
+            }catch (Exception e2)
+            {
+
+            }
+        }
     }
 }
